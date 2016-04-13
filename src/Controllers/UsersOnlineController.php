b4a5a64db016e4248a099af377a163da3c0ddfa3
gabriel.goncalves <gabriel.goncalves@fidelize.com.br>
2016-04-13 10:05:26 -0300

    * Change the controller to use the auth middleware

--- src/Controllers/UsersOnlineController.php
+++ src/Controllers/UsersOnlineController.php
@@ -7,6 +7,22 @@ use HighIdeas\UsersOnline\Models\UsersOnline;
 
 class UsersOnlineController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show all users online.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $users = UsersOnline::all();
